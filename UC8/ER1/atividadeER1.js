4ac830aaec6d3f88e9eed587d1dd501e1d321e44
Alerran Victor <alerransilva13@gmail.com>
2022-10-08 20:52:04 -0300

    Atividade ER1

---
+++ UC8/ER1/atividadeER1.js
@@ -0,0 +1,32 @@
+let listaDePecas = ['Filtro de ar', 'Roda', 'Pneu']
+
+listaDePecas.push('Disco de freio')
+listaDePecas.push('Motor')
+listaDePecas.push('Amortecedor')
+
+if (listaDePecas.length < 10){
+    console.log("É possível cadastrar mais peças!")
+}
+else{
+    console.log("Não há mais espaço na caixa")
+}
+
+let peso = 150
+if (peso < 100){
+    console.log("A peça deve pesar no mínimo 100g")
+}
+else{
+    console.log("A peça possui o peso adequado")
+}
+
+let nomePeca = "Disco de Freio"
+
+if (nomePeca.length > 3){
+    console.log("O nome da peça está adequado para o cadastro")
+}
+else if (nomePeca.length == 0){
+    console.log("O nome da peça não pode estar vazio")
+}
+else{
+    console.log("O nome da peça deve conter mais de três caracteres! Digite o nome adequado")
+}
